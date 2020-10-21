48ca54a243ef0b985e53ce5f8a99984ea8d6e1b3
SeaLoong <984391132@qq.com>
2020-10-21 14:11:24 +0800

    Update main.rs

--- references_borrowing/src/main.rs
+++ references_borrowing/src/main.rs
@@ -6,14 +6,13 @@ fn main() {
     /* 对任何类型 T 都有
      * &T 实现了 Copy<T> + Clone<T> + Deref<T> + Borrow<T> + Pointer<T>
      * &mut T 实现了 &T + DerefMut<T> + BorrowMut<T>
-     * 假设有变量 x，则没有方法从 &x/&mut x 来获取 x 的真正意义上的"所有权"
-     * 从 &x 只能获取 x.clone() 的 所有权，即其深拷贝的 所有权
-     * 从 &mut x 可以获取其(浅拷贝)的 所有权，以下是几种方式
+     * 从 &x 不能获取 所有权
+     * 从 &mut x 可以获取 所有权，以下是几种方式
      * std::mem::take(dest: &mut T) -> T
      * std::mem::replace(dest: &mut T, src: T) -> T
      */
     /* ================================= 所有权与借用的转换 ================================= */
-    let mut x: Vec<Vec<u8>> = vec![vec![1], vec![2]];
+    let mut x: Vec<Vec<u8>> = vec![vec![123], vec![223]];
     {
         /* 从 不可变借用 获取 所有权 */
         let a = &x;
@@ -21,38 +20,31 @@ fn main() {
         // let y = *a;
         // error[E0507]: cannot move out of `*a` which is behind a shared reference
         /* 总结
-         * 无法通过 不可变借用 获取原内存的 所有权
+         * 无法通过 不可变借用 获取 所有权
          */
     }
     {
         /* 从 可变借用 获取 所有权 */
-        let a = &mut x;
         // 记录 x 的内存地址
-        let p1 = a as *const Vec<Vec<u8>>;
-        // 记录 x 中的元素的内存地址
-        let p11 = &mut a[0] as *const Vec<u8>;
+        let p1 = x.as_ptr();
+        let a = &mut x;
         // 不能通过解引用获取 所有权
         // let y = *a;
         // error[E0507]: cannot move out of `*a` which is behind a mutable reference
         // std::mem::take 可行，std::mem::replace 可类似分析，以下不测试
         let mut y = std::mem::take(a);
-        let b = &mut y;
         // 记录 y 的内存地址
-        let p2 = b as *const Vec<Vec<u8>>;
-        // 记录 y 中的元素的内存地址
-        let p21 = &mut b[0] as *const Vec<u8>;
-        // p1 != p2 因此实际上也是发生了拷贝，并且此时 x, y 都能使用
+        let p2 = y.as_ptr();
+        let b = &mut y;
+        // p1 == p2 因此实际上是发生了指针的拷贝
+        assert_eq!(p1, p2);
+        // 此时 x 的地址已发生改变，y 的地址变为 x 原先的地址
+        let p1 = x.as_ptr();
         assert_ne!(p1, p2);
         println!("x = {:?}, y = {:?}", x, y);
-        // p11 == p21 结合上述可以知道实际上是发生了浅拷贝，由于 Rust 的设计，浅拷贝只能留存一份，否则会产生所有权的冲突
-        assert_eq!(p11, p21);
-        unsafe {
-            println!("p1 = {:?}, p2 = {:?}", *p1, *p2);
-            println!("p1 = {:?}, p2 = {:?}", *p11, *p21);
-        }
         /* 总结
          * 无法通过 可变借用 获取原内存的 所有权
-         * 可以通过 可变借用 获取其浅拷贝的所有权, 可以认为这是一种 (伪)所有权转移
+         * 可以通过 可变借用 获取所有权, 其实质是指针的剪切
          */
     }
     /* 由于 可变借用 只能从 可变绑定 中 获取，因此下列情况不会有包括这种情况的测试 */
